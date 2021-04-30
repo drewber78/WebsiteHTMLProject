9b7d0312455066ca4d0620e2758d10c6d66988f2
drewber78 <79421191+drewber78@users.noreply.github.com>
2021-04-30 09:15:54 -0700

    Creating new fork for website code
    Final project for the Forgotten Bastards of Group 8. Thanks Nik, Alessandro, and Jerry.

---
+++ FBDBtableinsert.php
@@ -0,0 +1,50 @@
+<?php
+
+/* Connect to the local server using AD DS Windows Authentication system */
+
+$serverName = "SQLServer1\\SQLEXPRESS";
+$connectionInfo = array("Database"=>"FBs_Big_Data", "UID"=>"FBadmin", "PWD"=>"ForgottenBastards!", "CharacterSet"=>"UTF-8");
+
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection Established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r(sqlsrv_errors(), true));
+}  
+
+/* Define the query */  
+$tsql1 = "INSERT INTO dbo.FB_Website_table (Groupname, Firstname, Lastname, Located, Datesubmitted)  
+           VALUES (?, ?, ?, ?, ?)";  
+
+/* Construct the parameter array from HTML variable inputs */  
+$Groupname = $_POST ["Groupname"];  
+$Firstname = $_POST ["Firstname"];  
+$Lastname = $_POST ["Lastname"];  
+$Located = $_POST ["Located"];
+$Datesubmitted = $_POST ["Datesubmitted"];
+
+$params1 = array(
+	array ($Groupname, null),
+	array($Firstname, null),
+	array($Lastname, null),
+	array($Located, null),
+	array($Datesubmitted , null),
+           );    
+
+/* Execute the INSERT query. */  
+$stmt1 = sqlsrv_query($conn, $tsql1, $params1);  
+if( $stmt1 === false )  
+{  
+     echo "Error in execution of INSERT.\n";  
+     die( print_r( sqlsrv_errors(), true));  
+}  
+if( $stmt1 ) 
+{
+     echo "Database updated.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
